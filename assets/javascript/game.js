bc27bf22fec6f2a777db997847c7704c7835a4cc
RepubIique <kendrickbong1996@gmail.com>
2019-09-30 15:05:01 +0800

    added the _ _ _ stuff into the js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,4 @@
-var rand = 0;
-var word = "";
-var songs = [
+var choice = [
   "Send me your location",
   "Yesterday all my troubles seemed so far away",
   "Let's make a break for the dawn",
@@ -9,27 +7,21 @@ var songs = [
   "I believed in nothing till I believed in you",
   "Nah nah nah nah nah nah nah, nah nah nah nah, hey Jude",
   "Is this the real life? Is this just fantasy?",
-  "I'm living in that twenty-first century",
-  "Have you ever seen the rain?",
-  "She's got eyes of the bluest skys",
-  "So close, no matter how far",
-  "I don't want to miss a thing",
-  "Take my hand, we'll make it, I swear",
-  "That's me in the corner",
-  "A mulatto, an albino, a mosquito, my libido",
-  "You've been thunderstruck",
-  "Another one bites the dust",
-  "Eye of the tiger",
-  "B-B-B-Bennie and the Jets",
-  "Sing us a song you're the piano man",
-  "Hello darkness my old friend",
-  "Somewhere over the rainbow blue birds fly",
-  "Don't worry about a thing",
-  "Imagine there's no heaven",
-  "Can you feel the love tonight?"
+  "I'm living in that twenty-first century"
 ];
+var rand = Math.floor(Math.random() * choice.length);
+var answers = choice[rand];
+var display = [answers];
+var word = "";
+var setup = function() {
+  for (var i = 0; i < answers.length; i++) {
+    display[i] = " _";
+    word = word + display[i];
+  }
+  document.getElementById("choice").innerHTML = word;
+  word = "";
+};
 
-function song() {
-  rand = Math.floor(Math.random() * songs.length);
-  word = songs[rand];
-}
+// window.onload = function() {
+//     setup();
+//   };
