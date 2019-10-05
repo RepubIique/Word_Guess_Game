9ba438b0b758c900d09f0eb8782f452d34418862
RepubIique <kendrickbong1996@gmail.com>
2019-10-05 22:23:21 +0800

    added objects to global variable

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,17 @@
-// create variables so that this Array can be stored all the letters not in currentGuessWord
-var usedLetterArray = [];
-// create variable that can be changed
-var currentGuessWord = "";
-var currentMaskedWord = "";
+var gameState = {
+  wins: 0,
+  tries: 7,
+  usedLetterArray: []
+};
 
-var tries = 7;
-var wins = 0;
+var gameVariables = {
+  currentGuessWord: "",
+  currentMaskedWord: ""
+};
+
+// var artist = {
+//   name:
+// };
 
 // When a key is pressed
 document.onkeyup = event => {
@@ -17,97 +23,99 @@ document.onkeyup = event => {
     keyPressed.length === 1
   ) {
     //if key pressed is used before
-    if (usedLetterArray.includes(keyPressed)) {
+    if (gameState.usedLetterArray.includes(keyPressed)) {
       alert("You have guessed this letter before, try something else.");
     } else {
       // run this function with "keyPressed" the keypressed
       validateGuess(keyPressed);
     }
   }
-  //updates the usedLetterArray
-  document.getElementById("usedLetters").innerHTML = usedLetterArray;
+  //updates the gameState.usedLetterArray
+  document.getElementById("usedLetters").innerHTML = gameState.usedLetterArray;
 };
 
 function tryCounter() {
   // When activated, increment counter by 1
-  tries--;
+  gameState.tries--;
   // if user made 7 guesses
-  if (tries === 0) {
+  if (gameState.tries === 0) {
     // reset game
     setNewGuess();
     alert("You've broke the game");
   }
   // updates the try counter
-  document.getElementById("tryCounter").innerHTML = tries.toString();
+  document.getElementById("tryCounter").innerHTML = gameState.tries.toString();
 }
 
 function setNewGuess() {
   // reset try counter to 0
-  tries = 7;
-  // reset the usedLetterArray
-  usedLetterArray = [];
+  gameState.tries = 7;
+  // reset the gameState.usedLetterArray
+  gameState.usedLetterArray = [];
   // possible answers
-  var phrases = [
-    "Yesterday all my troubles seemed so far away",
-    "Just a small town girl",
-    "I still haven't found what I'm looking for",
-    "Nah nah nah nah nah nah nah, nah nah nah nah, hey Jude",
-    "Is this the real life? Is this just fantasy?",
-    "I don't want to miss a thing",
-    "Take my hand, we'll make it, I swear",
-    "You've been thunderstruck",
-    "Sing us a song you're the piano man",
-    "Hello darkness my old friend",
-    "Can you feel the love tonight?"
+  var artist = [
+    "Taylor Swift",
+    "Jason Mraz",
+    "Avicii",
+    "The Beatles",
+    "Sigma",
+    "WestLife",
+    "Backstreet Boys"
   ];
-  // randomizes the answers in "phrases" array
-  currentGuessWord = phrases[Math.floor(Math.random() * phrases.length)];
+  // randomizes the answers in "artist" array
+  gameVariables.currentGuessWord =
+    artist[Math.floor(Math.random() * artist.length)];
   // This replace function uses Regex (Regular Expression) - https://en.wikipedia.org/wiki/Regular_expression
-  currentMaskedWord = currentGuessWord.replace(/[a-zA-Z]/g, "_");
-  // printing out the try counter and unknown phrases
-  document.getElementById("guessWord").innerHTML = currentMaskedWord;
-  document.getElementById("tryCounter").innerHTML = tries.toString();
+  gameVariables.currentMaskedWord = gameVariables.currentGuessWord.replace(
+    /[a-zA-Z]/g,
+    "_"
+  );
+  // printing out the try counter and unknown artist
+  document.getElementById("guessWord").innerHTML =
+    gameVariables.currentMaskedWord;
+  document.getElementById("tryCounter").innerHTML = gameState.tries.toString();
 }
 
 function validateGuess(keyPressed) {
   // when key is pressed, it makes sure that it is either a lower or uppercase
   if (
-    currentGuessWord.includes(keyPressed.toLowerCase()) ||
-    currentGuessWord.includes(keyPressed.toUpperCase())
+    gameVariables.currentGuessWord.includes(keyPressed.toLowerCase()) ||
+    gameVariables.currentGuessWord.includes(keyPressed.toUpperCase())
   ) {
     // checks keypressed against every single character of the word
-    for (var i = 0; i < currentGuessWord.length; i++) {
+    for (var i = 0; i < gameVariables.currentGuessWord.length; i++) {
       // replace "_" with keypressed if "i" is found to be the same
-      if (currentGuessWord[i].toLowerCase() == keyPressed) {
-        // replaces currentmaskedword with that letter
-        currentMaskedWord = replaceAt(
-          currentMaskedWord,
+      if (gameVariables.currentGuessWord[i].toLowerCase() == keyPressed) {
+        // replaces gameVariables.currentMaskedWord with that letter
+        gameVariables.currentMaskedWord = replaceAt(
+          gameVariables.currentMaskedWord,
           i,
-          currentGuessWord[i]
+          gameVariables.currentGuessWord[i]
         );
       }
     }
     // updates the guessWord
-    document.getElementById("guessWord").innerHTML = currentMaskedWord;
-    //runs win function
+    document.getElementById("guessWord").innerHTML =
+      gameVariables.currentMaskedWord;
+    //runs gameState.win function
     win();
   } else {
-    // if the key does not matches any letter in the phrase, it pushes that keypressed to the usedLetterArray
-    usedLetterArray.push(keyPressed);
+    // if the key does not matches any letter in the phrase, it pushes that keypressed to the gameState.usedLetterArray
+    gameState.usedLetterArray.push(keyPressed);
     // runs try counter
     tryCounter();
   }
 }
 
 function win() {
-  // if currentMaskedWord does not have "_" then alert done
+  // if gameVariables.currentMaskedWord does not have "_" then alert done
   // does not have is dictated by "!"
-  if (!currentMaskedWord.includes("_")) {
+  if (!gameVariables.currentMaskedWord.includes("_")) {
     alert("done");
     setNewGuess();
-    wins++;
+    gameState.wins++;
   }
-  document.getElementById("win").innerHTML = wins.toString();
+  document.getElementById("win").innerHTML = gameState.wins.toString();
 }
 
 //how does it work (https://www.w3schools.com/jsref/jsref_substring.asp)
